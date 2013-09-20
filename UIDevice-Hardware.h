4df9b41dd3594eb8aec824bb9950a639b3ed553b
Paul Fechner <pj.fechner@gmail.com>
2013-09-19 22:40:10 -0500

    Added several device differentials and made significant changes in order to simplify and streamline Hardware. Also, most of the work is done to change the methods from instance methods to class methods,

--- UIDevice-Hardware.h
+++ UIDevice-Hardware.h
@@ -4,84 +4,34 @@
  BSD License, Use at your own risk
  */
 
+/********************************************************
+ Drastic changes made by PeeJWeeJ, but thanks to all the contributors
+ *********************************************************/
+
 #import <UIKit/UIKit.h>
 
-#define IFPGA_NAMESTRING                @"iFPGA"
-
-#define IPHONE_1G_NAMESTRING            @"iPhone 1G"
-#define IPHONE_3G_NAMESTRING            @"iPhone 3G"
-#define IPHONE_3GS_NAMESTRING           @"iPhone 3GS" 
-#define IPHONE_4_NAMESTRING             @"iPhone 4"
-#define IPHONE_4S_NAMESTRING            @"iPhone 4S" 
-#define IPHONE_5_NAMESTRING             @"iPhone 5"
-#define IPHONE_UNKNOWN_NAMESTRING       @"Unknown iPhone"
-
-#define IPOD_1G_NAMESTRING              @"iPod touch 1G"
-#define IPOD_2G_NAMESTRING              @"iPod touch 2G"
-#define IPOD_3G_NAMESTRING              @"iPod touch 3G"
-#define IPOD_4G_NAMESTRING              @"iPod touch 4G"
-#define IPOD_UNKNOWN_NAMESTRING         @"Unknown iPod"
-
-#define IPAD_1G_NAMESTRING              @"iPad 1G"
-#define IPAD_2G_NAMESTRING              @"iPad 2G"
-#define IPAD_3G_NAMESTRING              @"iPad 3G"
-#define IPAD_UNKNOWN_NAMESTRING         @"Unknown iPad"
-
-#define APPLETV_2G_NAMESTRING           @"Apple TV 2G"
-#define APPLETV_UNKNOWN_NAMESTRING      @"Unknown Apple TV"
-
-#define IOS_FAMILY_UNKNOWN_DEVICE       @"Unknown iOS device"
-
-#define IPHONE_SIMULATOR_NAMESTRING         @"iPhone Simulator"
-#define IPHONE_SIMULATOR_IPHONE_NAMESTRING  @"iPhone Simulator"
-#define IPHONE_SIMULATOR_IPAD_NAMESTRING    @"iPad Simulator"
-
-typedef enum {
-    UIDeviceUnknown,
-    
-    UIDeviceiPhoneSimulator,
-    UIDeviceiPhoneSimulatoriPhone, // both regular and iPhone 4 devices
-    UIDeviceiPhoneSimulatoriPad,
-    
-    UIDevice1GiPhone,
-    UIDevice3GiPhone,
-    UIDevice3GSiPhone,
-    UIDevice4iPhone,
-    UIDevice4SiPhone,
-    UIDevice5iPhone,
-    
-    UIDevice1GiPod,
-    UIDevice2GiPod,
-    UIDevice3GiPod,
-    UIDevice4GiPod,
-    
-    UIDevice1GiPad,
-    UIDevice2GiPad,
-    UIDevice3GiPad,
-    
-    UIDeviceAppleTV2,
-    UIDeviceUnknownAppleTV,
-    
-    UIDeviceUnknowniPhone,
-    UIDeviceUnknowniPod,
-    UIDeviceUnknowniPad,
-    UIDeviceIFPGA,
-
-} UIDevicePlatform;
-
 @interface UIDevice (Hardware)
-- (NSString *) platform;
-- (NSString *) hwmodel;
-- (NSUInteger) platformType;
-- (NSString *) platformString;
 
-- (NSUInteger) cpuFrequency;
-- (NSUInteger) busFrequency;
-- (NSUInteger) totalMemory;
-- (NSUInteger) userMemory;
+///  Returns a string with the device name and if it's a retina device," Retina" will be added to the string. e.g. @"iPhone 5 Retina"
++ (NSString *) platformString;
 
-- (NSNumber *) totalDiskSpace;
-- (NSNumber *) freeDiskSpace;
+/// Returns the hw.model that is returned by the device
++ (NSString *) hwmodel;
 
-- (NSString *) macaddress;
+/// Evaluates as true if the device uses the phone idiom, for distinguishing between iPhone and iPad idioms. If you want to distinguish between iPhone and iPod, please use [UIDevice isIPod]
++ (BOOL) isPhoneIdiom;
+
+/// Evaluates as true only if the device is an iPOD. Use to distinguish between an iPhone and iPod. if you want to distinguish betwen iPad and iPhone interface idioms, use [UIDevice isPhoneIdiom
++ (BOOL) isIPod;
+
++ (NSUInteger) cpuFrequency;
++ (NSUInteger) busFrequency;
++ (NSUInteger) totalMemory;
++ (NSUInteger) userMemory;
+
++ (NSNumber *) totalDiskSpace;
++ (NSNumber *) freeDiskSpace;
+
++ (NSString *) macaddress;
+
 @end
--- UIDevice-Hardware.m
+++ UIDevice-Hardware.m
@@ -6,6 +6,10 @@
 
 // Thanks to Emanuele Vulcano, Kevin Ballard/Eridius, Ryandjohnson, Matt Brown, etc.
 
+/********************************************************
+ Drastic changes made by PeeJWeeJ, but thanks to all the contributers listed above and below
+ *********************************************************/
+
 #include <sys/socket.h> // Per msqr
 #include <sys/sysctl.h>
 #include <net/if.h>
@@ -13,43 +17,185 @@
 
 #import "UIDevice-Hardware.h"
 
+/********************************************************
+	Strings returned by the system
+ *********************************************************/
+#define IFPGA_SYS_STRING				@"iFPGA"
+
+#define IPHONE_PREFIX_SYS_STRING		@"iPhone"
+#define IPHONE_1_SYS_STRING				@"iPhone1,1"
+#define IPHONE_3G_SYS_STRING            @"iPhone1,2"
+#define IPHONE_3GS_SYS_STRING			@"iPhone2"
+#define IPHONE_4GSM_SYS_STRING			@"iPhone3,1"
+#define IPHONE_4CDMA_SYS_STRING			@"iPhone3,3"
+#define IPHONE_4S_SYS_STRING            @"iPhone4"
+#define IPHONE_5GSM_SYS_STRING			@"iPhone5,1"
+#define IPHONE_5CDMA_SYS_STRING			@"iPhone5,2"
+#define IPHONE_5S_SYS_STRING            @"iPhone??"
+#define IPHONE_5C_SYS_STRING            @"iPhone??"
+
+
+#define IPOD_PREFIX_SYS_STRING			@"iPod"
+#define IPOD_1G_SYS_STRING				@"iPod1"
+#define IPOD_2G_SYS_STRING				@"iPod2"
+#define IPOD_3G_SYS_STRING				@"iPod3"
+#define IPOD_4G_SYS_STRING				@"iPod4"
+#define IPOD_5G_SYS_STRING				@"iPod5"
+
+#define IPAD_PREFIX_SYS_STRING			@"iPad"
+#define IPAD_1_SYS_STRING				@"iPad1"
+
+#define IPAD_2_WIFI_SYS_STRING			@"iPad2,1"
+#define IPAD_2_GSM_SYS_STRING			@"iPad2,2"
+#define IPAD_2_CDMA_SYS_STRING			@"iPad2,3"
+#define IPAD_2_WIFI2_SYS_STRING			@"iPad2,4"
+
+#define IPAD_MINI_WIFI_SYS_STRING		@"iPad2,5"
+#define IPAD_MINI_GSM_SYS_STRING		@"iPad2,6"
+#define IPAD_MINI_CDMA_SYS_STRING		@"iPad2,7"
+
+#define IPAD_3_WIFI_SYS_STRING			@"iPad3,1"
+#define IPAD_3_CDMA_SYS_STRING			@"iPad3,2"
+#define IPAD_3_GSM_SYS_STRING			@"iPad3,3"
+
+#define IPAD_4_WIFI_SYS_STRING			@"iPad3,4"
+#define IPAD_4_GSM_SYS_STRING			@"iPad3,5"
+#define IPAD_4_CDMA_SYS_STRING			@"iPad3,6"
+
+
+
+
+/********************************************************
+ Strings [UIDevice platformString] will return
+ *********************************************************/
+
+#define IFPGA_NAMESTRING                @"iFPGA"
+
+#define IPHONE_1G_NAMESTRING            @"iPhone 1G"
+#define IPHONE_3G_NAMESTRING            @"iPhone 3G"
+#define IPHONE_3GS_NAMESTRING           @"iPhone 3GS"
+#define IPHONE_4_NAMESTRING             @"iPhone 4"
+#define IPHONE_4S_NAMESTRING            @"iPhone 4S"
+#define IPHONE_5_NAMESTRING             @"iPhone 5"
+#define IPHONE_5S_NAMESTRING            @"iPhone 5S"
+#define IPHONE_5C_NAMESTRING            @"iPhone 5C"
+#define IPHONE_UNKNOWN_NAMESTRING       @"Unknown iPhone"
+
+#define IPOD_1G_NAMESTRING              @"iPod touch 1G"
+#define IPOD_2G_NAMESTRING              @"iPod touch 2G"
+#define IPOD_3G_NAMESTRING              @"iPod touch 3G"
+#define IPOD_4G_NAMESTRING              @"iPod touch 4G"
+#define IPOD_5G_NAMESTRING              @"iPod touch 5G"
+#define IPOD_UNKNOWN_NAMESTRING         @"Unknown iPod"
+
+#define IPAD_1G_NAMESTRING              @"iPad 1G"
+#define IPAD_2G_NAMESTRING              @"iPad 2G"
+#define IPAD_3G_NAMESTRING              @"iPad 3G"
+#define IPAD_4G_NAMESTRING              @"iPad 4G"
+#define IPAD_1G_MINI_NAMESTRING			@"iPad Mini"
+#define IPAD_UNKNOWN_NAMESTRING         @"Unknown iPad"
+
+#define APPLETV_2G_NAMESTRING           @"Apple TV 2G"
+#define APPLETV_3G_NAMESTRING           @"Apple TV 3G"
+#define APPLETV_4G_NAMESTRING           @"Apple TV 4G"
+#define APPLETV_UNKNOWN_NAMESTRING      @"Unknown Apple TV"
+
+#define IOS_FAMILY_UNKNOWN_DEVICE       @"Unknown iOS device"
+
+#define SIMULATOR_NAMESTRING            @"Device Simulator"
+#define SIMULATOR_IPHONE_NAMESTRING     @"iPhone Simulator"
+#define SIMULATOR_IPAD_NAMESTRING       @"iPad Simulator"
+#define SIMULATOR_APPLETV_NAMESTRING    @"Apple TV Simulator"
+
+// rather than a huge if block or switch statement, I changed it to a dictinary with all the possible values. this doesn't look pretty, but it's a lot simpler to look at in the code
+// as it's a dictionary, it doesn't matter where a new device is added, so in future just make sure it's documented in the comments below
+#define STRINGS_DICTIONARY @{IFPGA_SYS_STRING: IFPGA_NAMESTRING,IPHONE_1_SYS_STRING: IPHONE_1G_NAMESTRING,IPHONE_3G_SYS_STRING: IPHONE_3G_NAMESTRING,IPHONE_3GS_SYS_STRING: IPHONE_3GS_NAMESTRING,IPHONE_4GSM_SYS_STRING: IPHONE_4_NAMESTRING,IPHONE_4CDMA_SYS_STRING: IPHONE_4_NAMESTRING,IPHONE_4S_SYS_STRING: IPHONE_4S_NAMESTRING,IPHONE_5GSM_SYS_STRING: IPHONE_5_NAMESTRING,IPHONE_5CDMA_SYS_STRING: IPHONE_5_NAMESTRING,IPHONE_5S_SYS_STRING: IPHONE_5S_NAMESTRING,IPHONE_5C_SYS_STRING: IPHONE_5C_NAMESTRING,IPOD_1G_SYS_STRING: IPOD_1G_NAMESTRING,IPOD_2G_SYS_STRING: IPOD_2G_NAMESTRING,IPOD_3G_SYS_STRING: IPOD_3G_NAMESTRING,IPOD_4G_SYS_STRING: IPOD_4G_NAMESTRING,IPOD_5G_SYS_STRING: IPOD_5G_NAMESTRING,IPAD_1_SYS_STRING: IPAD_1G_NAMESTRING,IPAD_2_WIFI_SYS_STRING: IPAD_2G_NAMESTRING,IPAD_2_GSM_SYS_STRING: IPAD_2G_NAMESTRING,IPAD_2_CDMA_SYS_STRING: IPAD_2G_NAMESTRING,IPAD_2_WIFI2_SYS_STRING: IPAD_2G_NAMESTRING,IPAD_MINI_WIFI_SYS_STRING: IPAD_1G_MINI_NAMESTRING,IPAD_MINI_GSM_SYS_STRING: IPAD_1G_MINI_NAMESTRING,IPAD_MINI_CDMA_SYS_STRING: IPAD_1G_MINI_NAMESTRING,IPAD_3_WIFI_SYS_STRING: IPAD_3G_NAMESTRING,IPAD_3_CDMA_SYS_STRING: IPAD_3G_NAMESTRING,IPAD_3_GSM_SYS_STRING: IPAD_3G_NAMESTRING,IPAD_4_WIFI_SYS_STRING: IPAD_4G_NAMESTRING,IPAD_4_GSM_SYS_STRING: IPAD_4G_NAMESTRING,IPAD_4_CDMA_SYS_STRING: IPAD_4G_NAMESTRING};
+
+/*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ More organized view of the info above. Please add to both
+ 
+STRINGS_DICTIONARY @{
+ IFPGA_SYS_STRING: IFPGA_NAMESTRING,				IPHONE_1_SYS_STRING: IPHONE_1G_NAMESTRING,
+ IPHONE_3G_SYS_STRING: IPHONE_3G_NAMESTRING,		IPHONE_3GS_SYS_STRING: IPHONE_3GS_NAMESTRING,
+ IPHONE_4GSM_SYS_STRING: IPHONE_4_NAMESTRING,		IPHONE_4CDMA_SYS_STRING: IPHONE_4_NAMESTRING,
+ IPHONE_4S_SYS_STRING: IPHONE_4S_NAMESTRING,		IPHONE_5GSM_SYS_STRING: IPHONE_5_NAMESTRING,
+ IPHONE_5CDMA_SYS_STRING: IPHONE_5_NAMESTRING,		IPHONE_5S_SYS_STRING: IPHONE_5S_NAMESTRING,
+ IPHONE_5C_SYS_STRING: IPHONE_5C_NAMESTRING,		IPOD_1G_SYS_STRING: IPOD_1G_NAMESTRING,
+ IPOD_2G_SYS_STRING: IPOD_2G_NAMESTRING,			IPOD_3G_SYS_STRING: IPOD_3G_NAMESTRING,
+ IPOD_4G_SYS_STRING: IPOD_4G_NAMESTRING,			IPOD_5G_SYS_STRING: IPOD_5G_NAMESTRING,
+ IPAD_1_SYS_STRING: IPAD_1G_NAMESTRING,				IPAD_2_WIFI_SYS_STRING: IPAD_2G_NAMESTRING,
+ IPAD_2_GSM_SYS_STRING: IPAD_2G_NAMESTRING,			IPAD_2_CDMA_SYS_STRING: IPAD_2G_NAMESTRING,
+ IPAD_2_WIFI2_SYS_STRING: IPAD_2G_NAMESTRING,		IPAD_MINI_WIFI_SYS_STRING: IPAD_1G_MINI_NAMESTRING,
+ IPAD_MINI_GSM_SYS_STRING: IPAD_1G_MINI_NAMESTRING,	IPAD_MINI_CDMA_SYS_STRING: IPAD_1G_MINI_NAMESTRING,
+ IPAD_3_WIFI_SYS_STRING: IPAD_3G_NAMESTRING,		IPAD_3_CDMA_SYS_STRING: IPAD_3G_NAMESTRING,
+ IPAD_3_GSM_SYS_STRING: IPAD_3G_NAMESTRING,			IPAD_4_WIFI_SYS_STRING: IPAD_4G_NAMESTRING,
+ IPAD_4_GSM_SYS_STRING: IPAD_4G_NAMESTRING,			IPAD_4_CDMA_SYS_STRING: IPAD_4G_NAMESTRING};
+ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ */
 @implementation UIDevice (Hardware)
-/*
- Platforms
- 
- iFPGA ->        ??
 
- iPhone1,1 ->    iPhone 1G, M68
- iPhone1,2 ->    iPhone 3G, N82
- iPhone2,1 ->    iPhone 3GS, N88
- iPhone3,1 ->    iPhone 4/AT&T, N89
- iPhone3,2 ->    iPhone 4/Other Carrier?, ??
- iPhone3,3 ->    iPhone 4/Verizon, TBD
- iPhone4,1 ->    (iPhone 5/AT&T), TBD
- iPhone4,2 ->    (iPhone 5/Verizon), TBD
+#pragma mark platform type and name utils
 
- iPod1,1   ->    iPod touch 1G, N45
- iPod2,1   ->    iPod touch 2G, N72
- iPod2,2   ->    Unknown, ??
- iPod3,1   ->    iPod touch 3G, N18
- iPod4,1   ->    iPod touch 4G, N80
- 
- // Thanks NSForge
- iPad1,1   ->    iPad 1G, WiFi and 3G, K48
- iPad2,1   ->    iPad 2G, WiFi, K93
- iPad2,2   ->    iPad 2G, GSM 3G, K94
- iPad2,3   ->    iPad 2G, CDMA 3G, K95
- iPad3,1   ->    (iPad 3G, GSM)
- iPad3,2   ->    (iPad 3G, CDMA)
+/********************************************************
+ Returns a string with the device name and if it's a retina device," Retina" will be added to the string.
+ e.g. @"iPhone 5 Retina"
+ *********************************************************/
++ (NSString *) platformString
+{
+	NSString *hwString = [self getSysInfoByName:"hw.machine"];
+	NSString *platform;
+	
+	NSDictionary *newDictionary = STRINGS_DICTIONARY;
+	platform = [newDictionary objectForKey:hwString];
+	
+	if(!platform){
+		if ([hwString hasPrefix:IPHONE_PREFIX_SYS_STRING])	platform = IPHONE_UNKNOWN_NAMESTRING;
+		else if ([hwString hasPrefix:IPOD_PREFIX_SYS_STRING])	platform = IPOD_UNKNOWN_NAMESTRING;
+		else if ([hwString hasPrefix:IPAD_PREFIX_SYS_STRING])	platform = IPAD_UNKNOWN_NAMESTRING;
+		
+		
+		
+		// Apple TV
+		else if ([hwString hasPrefix:@"AppleTV"]){
+			if ([hwString hasPrefix:@"AppleTV2"])           platform = APPLETV_2G_NAMESTRING;
+			else if ([hwString hasPrefix:@"AppleTV3"])      platform = APPLETV_3G_NAMESTRING;
+			else if	([hwString hasPrefix:@"AppleTV34"])  	platform = APPLETV_4G_NAMESTRING;
+			else											platform = SIMULATOR_APPLETV_NAMESTRING;
+		}
+		
+		// Simulator
+		else if ([hwString hasSuffix:@"86"] || [hwString isEqual:@"x86_64"]){
+			platform = ([UIDevice isPhoneIdiom])? SIMULATOR_IPHONE_NAMESTRING : SIMULATOR_IPAD_NAMESTRING;
+		}
+		else platform = IOS_FAMILY_UNKNOWN_DEVICE;
+	}
+	
+	
+	
+	if(([UIScreen mainScreen].scale == 2.0f)){
+		platform = [NSString stringWithFormat:@"%@ Retina", platform];
+	}
+	
+	return platform;
+}
 
- AppleTV2,1 ->   AppleTV 2, K66
+// returns true if it's an Phone
++ (BOOL) isPhoneIdiom
+{
+	return ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone)? YES : NO;
+}
 
- i386, x86_64 -> iPhone Simulator
-*/
+// returns true only if it's an iPod
++ (BOOL) isIPod;
 
+{
+	NSString *hwString = [self getSysInfoByName:"hw.machine"];
+	
+	return ([hwString hasPrefix:IPOD_PREFIX_SYS_STRING])? YES : NO;
+}
 
 #pragma mark sysctlbyname utils
-- (NSString *) getSysInfoByName:(char *)typeSpecifier
++ (NSString *) getSysInfoByName:(char *)typeSpecifier
 {
     size_t size;
     sysctlbyname(typeSpecifier, NULL, &size, NULL, 0);
@@ -63,20 +209,19 @@
     return results;
 }
 
-- (NSString *) platform
++ (NSString *) platform
 {
     return [self getSysInfoByName:"hw.machine"];
 }
 
-
 // Thanks, Tom Harrington (Atomicbird)
-- (NSString *) hwmodel
++ (NSString *) hwmodel
 {
     return [self getSysInfoByName:"hw.model"];
 }
 
 #pragma mark sysctl utils
-- (NSUInteger) getSysInfo: (uint) typeSpecifier
++ (NSUInteger) getSysInfo: (uint) typeSpecifier
 {
     size_t size = sizeof(int);
     int results;
@@ -85,129 +230,49 @@
     return (NSUInteger) results;
 }
 
-- (NSUInteger) cpuFrequency
++ (NSUInteger) cpuFrequency
 {
     return [self getSysInfo:HW_CPU_FREQ];
 }
 
-- (NSUInteger) busFrequency
++ (NSUInteger) busFrequency
 {
     return [self getSysInfo:HW_BUS_FREQ];
 }
 
-- (NSUInteger) totalMemory
++ (NSUInteger) totalMemory
 {
     return [self getSysInfo:HW_PHYSMEM];
 }
 
-- (NSUInteger) userMemory
++ (NSUInteger) userMemory
 {
     return [self getSysInfo:HW_USERMEM];
 }
 
-- (NSUInteger) maxSocketBufferSize
++ (NSUInteger) maxSocketBufferSize
 {
     return [self getSysInfo:KIPC_MAXSOCKBUF];
 }
 
 #pragma mark file system -- Thanks Joachim Bean!
-- (NSNumber *) totalDiskSpace
++ (NSNumber *) totalDiskSpace
 {
     NSDictionary *fattributes = [[NSFileManager defaultManager] attributesOfFileSystemForPath:NSHomeDirectory() error:nil];
     return fattributes[NSFileSystemSize];
 }
 
-- (NSNumber *) freeDiskSpace
++ (NSNumber *) freeDiskSpace
 {
     NSDictionary *fattributes = [[NSFileManager defaultManager] attributesOfFileSystemForPath:NSHomeDirectory() error:nil];
     return fattributes[NSFileSystemFreeSize];
-}
-
-#pragma mark platform type and name utils
-- (NSUInteger) platformType
-{
-    NSString *platform = [self platform];
-
-    // The ever mysterious iFPGA
-    if ([platform isEqualToString:@"iFPGA"])        return UIDeviceIFPGA;
-
-    // iPhone
-    if ([platform isEqualToString:@"iPhone1,1"])    return UIDevice1GiPhone;
-    if ([platform isEqualToString:@"iPhone1,2"])    return UIDevice3GiPhone;
-    if ([platform hasPrefix:@"iPhone2"])            return UIDevice3GSiPhone;
-    if ([platform hasPrefix:@"iPhone3"])            return UIDevice4iPhone;
-    if ([platform hasPrefix:@"iPhone4"])            return UIDevice4SiPhone;
-    if ([platform hasPrefix:@"iPhone5"])            return UIDevice5iPhone;
-    
-    // iPod
-    if ([platform hasPrefix:@"iPod1"])             return UIDevice1GiPod;
-    if ([platform hasPrefix:@"iPod2"])              return UIDevice2GiPod;
-    if ([platform hasPrefix:@"iPod3"])              return UIDevice3GiPod;
-    if ([platform hasPrefix:@"iPod4"])              return UIDevice4GiPod;
-
-    // iPad
-    if ([platform hasPrefix:@"iPad1"])              return UIDevice1GiPad;
-    if ([platform hasPrefix:@"iPad2"])              return UIDevice2GiPad;
-    if ([platform hasPrefix:@"iPad3"])              return UIDevice3GiPad;
-    
-    // Apple TV
-    if ([platform hasPrefix:@"AppleTV2"])           return UIDeviceAppleTV2;
-
-    if ([platform hasPrefix:@"iPhone"])             return UIDeviceUnknowniPhone;
-    if ([platform hasPrefix:@"iPod"])               return UIDeviceUnknowniPod;
-    if ([platform hasPrefix:@"iPad"])               return UIDeviceUnknowniPad;
-    
-     // Simulator thanks Jordan Breeding
-    if ([platform hasSuffix:@"86"] || [platform isEqual:@"x86_64"])
-    {
-        BOOL smallerScreen = [[UIScreen mainScreen] bounds].size.width < 768;
-        return smallerScreen ? UIDeviceiPhoneSimulatoriPhone : UIDeviceiPhoneSimulatoriPad;
-    }
-
-    return UIDeviceUnknown;
-}
-
-- (NSString *) platformString
-{
-    switch ([self platformType])
-    {
-        case UIDevice1GiPhone: return IPHONE_1G_NAMESTRING;
-        case UIDevice3GiPhone: return IPHONE_3G_NAMESTRING;
-        case UIDevice3GSiPhone: return IPHONE_3GS_NAMESTRING;
-        case UIDevice4iPhone: return IPHONE_4_NAMESTRING;
-        case UIDevice4SiPhone: return IPHONE_4S_NAMESTRING;
-        case UIDevice5iPhone: return IPHONE_5_NAMESTRING;
-        case UIDeviceUnknowniPhone: return IPHONE_UNKNOWN_NAMESTRING;
-        
-        case UIDevice1GiPod: return IPOD_1G_NAMESTRING;
-        case UIDevice2GiPod: return IPOD_2G_NAMESTRING;
-        case UIDevice3GiPod: return IPOD_3G_NAMESTRING;
-        case UIDevice4GiPod: return IPOD_4G_NAMESTRING;
-        case UIDeviceUnknowniPod: return IPOD_UNKNOWN_NAMESTRING;
-            
-        case UIDevice1GiPad : return IPAD_1G_NAMESTRING;
-        case UIDevice2GiPad : return IPAD_2G_NAMESTRING;
-        case UIDevice3GiPad : return IPAD_3G_NAMESTRING;
-        case UIDeviceUnknowniPad : return IPAD_UNKNOWN_NAMESTRING;
-            
-        case UIDeviceAppleTV2 : return APPLETV_2G_NAMESTRING;
-        case UIDeviceUnknownAppleTV: return APPLETV_UNKNOWN_NAMESTRING;
-            
-        case UIDeviceiPhoneSimulator: return IPHONE_SIMULATOR_NAMESTRING;
-        case UIDeviceiPhoneSimulatoriPhone: return IPHONE_SIMULATOR_IPHONE_NAMESTRING;
-        case UIDeviceiPhoneSimulatoriPad: return IPHONE_SIMULATOR_IPAD_NAMESTRING;
-            
-        case UIDeviceIFPGA: return IFPGA_NAMESTRING;
-            
-        default: return IOS_FAMILY_UNKNOWN_DEVICE;
-    }
 }
 
 #pragma mark MAC addy
 // Return the local MAC addy
 // Courtesy of FreeBSD hackers email list
 // Accidentally munged during previous update. Fixed thanks to mlamb.
-- (NSString *) macaddress
++ (NSString *) macaddress
 {
     int                 mib[6];
     size_t              len;
@@ -248,21 +313,10 @@
     ptr = (unsigned char *)LLADDR(sdl);
     NSString *outstring = [NSString stringWithFormat:@"%02X:%02X:%02X:%02X:%02X:%02X", 
                            *ptr, *(ptr+1), *(ptr+2), *(ptr+3), *(ptr+4), *(ptr+5)];
-    // NSString *outstring = [NSString stringWithFormat:@"%02X%02X%02X%02X%02X%02X", 
-    //                       *ptr, *(ptr+1), *(ptr+2), *(ptr+3), *(ptr+4), *(ptr+5)];
     free(buf);
 
     return outstring;
 }
 
-// Illicit Bluetooth check -- cannot be used in App Store
-/* 
-Class  btclass = NSClassFromString(@"GKBluetoothSupport");
-if ([btclass respondsToSelector:@selector(bluetoothStatus)])
-{
-    printf("BTStatus %d\n", ((int)[btclass performSelector:@selector(bluetoothStatus)] & 1) != 0);
-    bluetooth = ((int)[btclass performSelector:@selector(bluetoothStatus)] & 1) != 0;
-    printf("Bluetooth %s enabled\n", bluetooth ? "is" : "isn't");
-}
-*/
+
 @end
